4c42148423ff8fd8c6babfaf6448aa886177cdcf
Natalia Gilyazova <101715509+Natalia1607@users.noreply.github.com>
2022-11-12 15:12:17 +0300

    -

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-/* const link = "http://api.weatherstack.com/current?access_key=3f28eddde2a4b6965b1f9ab43af34942"; //8889d57647145852e079cecbff2427a1
- */
+const link = "http://api.weatherstack.com/current?access_key=3f28eddde2a4b6965b1f9ab43af34942"; //8889d57647145852e079cecbff2427a1
+ 
 const root = document.getElementById('root');
 const popup = document.getElementById('popup');
 const textInput = document.getElementById('text-input');
